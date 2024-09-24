fb7fc3695c8f64b79a7dbb87d96645732f3bbb07
douglas ribeiro <dhribeiro@apoioprodesp.sp.gov.br>
2024-09-23 21:40:05 -0300

    Atualizando server.js para funcionar no Glitch

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Use a porta dinâmica do Glitch
 
 // Configuração do Multer para armazenar imagens no diretório "uploads"
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, 'uploads/'); // Certifique-se que a pasta uploads existe
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -23,7 +23,7 @@ const upload = multer({ storage: storage }).fields([
 ]);
 
 // Middleware para servir arquivos estáticos (HTML, CSS, etc.)
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // Certifique-se de que os arquivos estáticos estão acessíveis
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
