26060e1723880cdfc7fe9a28aced1acbf504c6f4
douglashr <douglashr3@gmail.com>
2025-01-08 09:54:59 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@
 						if ($this.height() <= wh)
 							$this.css('height', (wh - 50) + 'px');
 					});
-				}, 50);
+				});
 			})
 			.triggerHandler('resize.ie-flexbox-fix');
 	}
@@ -144,20 +144,24 @@
 
 	$window.on('load resize', ajustarAltura);
 
-	// Eventos de scroll suave.
-	$('a[href^="#"]').scrolly({
-		speed: 1500,
-		offset: $header.outerHeight() - 1
+	// Events.
+	var resizeTimeout;
+	$window.on('resize', function() {
+		$body.addClass('is-resizing');
+		clearTimeout(resizeTimeout);
+		resizeTimeout = setTimeout(function() {
+			$('a[href^="#"]').scrolly({
+				speed: 1500,
+				offset: $header.outerHeight() - 1
+			});
+			setTimeout(function() {
+				$body.removeClass('is-resizing');
+				$window.trigger('scroll');
+			}, 0);
+		}, 100);
 	});
-
-	// Reinicializa plugins após alterações dinâmicas.
-	function reinicializarTudo() {
-		console.log("Reinicializando tudo...");
-		inicializarGaleria();
-		inicializarAnimacoes();
-	}
-
-	// Eventos para recarregar animações após mudanças dinâmicas.
-	$(document).on('conteudoAtualizado', reinicializarTudo);
+	$window.on('load', function() {
+		$window.trigger('resize');
+	});
 
 })(jQuery);
